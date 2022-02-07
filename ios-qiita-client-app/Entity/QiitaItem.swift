444dc44c0d4d6486e8aba28b9720c6090b8869d5
steel <git config --global user.email>
2022-02-07 14:56:38 +0900

    feat: QiitaRepositoryの追加とテストの追加&テストできるように修正

--- ios-qiita-client-app/Entity/QiitaItem.swift
+++ ios-qiita-client-app/Entity/QiitaItem.swift
@@ -8,7 +8,11 @@
 import Foundation
 import UIKit
 
-struct QiitaItem: Decodable {
+struct QiitaItem: Decodable, Equatable {
+    static func == (lhs: QiitaItem, rhs: QiitaItem) -> Bool {
+        return true
+    }
+    
     let title: String
     let id: String
     let user: User
@@ -16,7 +20,6 @@ struct QiitaItem: Decodable {
 
 struct User: Decodable {
     let thumbnailUrl: String
-    
     enum CodingKeys: String, CodingKey {
         case thumbnailUrl = "profile_image_url"
     }
--- ios-qiita-client-app/GateWay/ApiService/QiitaAPI.swift
+++ ios-qiita-client-app/GateWay/ApiService/QiitaAPI.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import Alamofire
+
+typealias ResultHandler<T> = (Result<T, Error>) -> Void
+
+protocol QiitaApiServiceProtocol {
+    func getQiitaItems() async throws -> [QiitaItem]
+}
+
+class QiitaApiService{
+    let BASE_URL = "https://qiita.com/api/v2"
+    func getQiitaItems() async throws -> [QiitaItem]{
+       let result = try await AF.request("\(BASE_URL)/items").publish([QiitaItem].self)
+        return result
+    }
+}
+
+extension DataRequest {
+    func publish<T>(_ type: T.Type) async throws -> T where T : Decodable {
+            try await withCheckedThrowingContinuation { continuation in
+                self.response { response in
+                    switch response.result {
+                    case .success(let element): do {
+                        let decodedResponse = try JSONDecoder().decode(type, from: element!)
+                        continuation.resume(returning: decodedResponse)
+                    } catch {
+                        continuation.resume(throwing: error)
+                    }
+                    case .failure(let error):
+                        continuation.resume(throwing: error)
+                    }
+                }
+            }
+    }
+}
