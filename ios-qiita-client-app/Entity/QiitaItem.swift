0668f150cca2f50e816a68270d5c627be77400e6
steel <git config --global user.email>
2022-02-08 18:00:48 +0900

    refactor: Equatableの理由をコメントで記載

--- ios-qiita-client-app/Entity/QiitaItem.swift
+++ ios-qiita-client-app/Entity/QiitaItem.swift
@@ -9,13 +9,19 @@ import Foundation
 import UIKit
 
 struct QiitaItem: Decodable, Equatable {
-    static func == (_: QiitaItem, _: QiitaItem) -> Bool {
-        return true
-    }
 
     let title: String
     let id: String
     let user: User
+    
+    
+    //
+    //XcTestのために準拠させる必要があるだけなので、実装に使われることはない
+    //PRおダクトに使用するならこの辺考える必要ありhttps://qiita.com/uhooi/items/638a6116278a3e591773
+    //
+    static func == (_rItem: QiitaItem, _lItem: QiitaItem) -> Bool {
+        return _rItem.title == _lItem.title && _rItem.id == _lItem.id && _rItem.user.thumbnailUrl == _lItem.user.thumbnailUrl
+    }
 }
 
 struct User: Decodable {
